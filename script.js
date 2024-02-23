947118cba6e957a73a70fdad5243162fc8c2952f
Prashin Jignesh Parikh <prashinparikh99@gmail.com>
2024-02-24 00:28:25 +0530

    Styled little better and fixed broken links, used javascript according to ECMAScript 2020 standards

--- script.js
+++ script.js
@@ -5,24 +5,24 @@
     function handleFormSubmit(event) {
         event.preventDefault();
         // 1. Storing the values of user input
-        var destName = event.target.elements["name"].value;
-        var destLocation = event.target.elements["location"].value;
-        var destPhoto = event.target.elements["photo"].value;
-        var destDesc = event.target.elements["description"].value;
+        let destName = event.target.elements["name"].value;
+        let destLocation = event.target.elements["location"].value;
+        let destPhoto = event.target.elements["photo"].value;
+        let destDesc = event.target.elements["description"].value;
 
         // 2. Clearing out the form fields after we have stored them
-        for (var i = 0; i < detailsForm.length; i++) {
+        for (let i = 0; i < detailsForm.length; i++) {
             detailsForm.elements[i].value = "";
         }
 
         // 3. Create a function that creates a new card
-        var vacationContainer = document.querySelector("#destinations_container");
+        let vacationContainer = document.querySelector("#destinations_container");
         if (vacationContainer.children.length === 0) {
-            var sideHeading = document.getElementById("title");
+            let sideHeading = document.getElementById("title");
             sideHeading.innerText = "My Wish List";
         }
         // Below function "createCard" returns HTML
-        var destCard = createCard(destName, destLocation, destPhoto, destDesc);
+        let destCard = createCard(destName, destLocation, destPhoto, destDesc);
 
         // So we append destCard to our main div in html file with id = destinations_container
         document.getElementById('destinations_container').appendChild(destCard);
@@ -34,7 +34,7 @@
 
         let img = document.createElement('img');
         img.setAttribute('alt', name);
-        const constantPhoto = `images/signpost.jpg`;
+        const constantPhoto = `./images/signpost.jpg`;
         if (photoUrl.length === 0) {
             img.setAttribute('src', constantPhoto);
         }
@@ -44,25 +44,25 @@
 
         card.appendChild(img);
 
-        var cardBody = document.createElement('div');
+        let cardBody = document.createElement('div');
         cardBody.className = 'card-body';
 
-        var cardTitle = document.createElement('h3');
+        let cardTitle = document.createElement('h3');
         cardTitle.innerText = name;
         cardBody.appendChild(cardTitle);
 
-        var cardSubTitle = document.createElement('h4');
+        let cardSubTitle = document.createElement('h4');
         cardSubTitle.innerText = loc;
         cardBody.appendChild(cardSubTitle);
 
         if (desc.length !== 0) {
-            var cardText = document.createElement('p');
+            let cardText = document.createElement('p');
             cardText.className = 'card-text';
             cardText.innerText = desc;
             cardBody.appendChild(cardText);
         }
 
-        var cardDeleteBtn = document.createElement("button");
+        let cardDeleteBtn = document.createElement("button");
         cardDeleteBtn.innerText = 'Remove';
 
         cardDeleteBtn.addEventListener('click', removeDestination);
@@ -73,7 +73,7 @@
     }
 
     function removeDestination(event) {
-        var card = event.target.parentElement.parentElement;
+        let card = event.target.parentElement.parentElement;
         card.remove();
     }
 
